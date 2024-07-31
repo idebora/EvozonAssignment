f62d700297a111a9bc24ae81aed75eae951cdc12
Deb <mdmaxinas@gmail.com>
2024-07-31 20:24:23 +0300

    Assignment 1 Java

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("Debora");
+
+        //ex 2
+
+        int a = 2;
+        int b = 3;
+        System.out.println(a + b);
+
+        //ex 3
+
+        System.out.println((float)a/b);
+
+        // ex 4
+
+        System.out.println(-5 + 8 * 6);
+        System.out.println((float) (55 + 9) % 9);
+        System.out.println((float) 20 + -3 * 5 / 8);
+        System.out.println((float) 5 + 15 / 3 * 2 - 8 % 3);
+
+        }
+    }
